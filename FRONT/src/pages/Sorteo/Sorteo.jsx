26544d5688f277feba330f8f6ef2743a764643f5
Wida30 <wida30@yahoo.es>
2022-06-29 20:22:45 +0200

    fotos

---
+++ FRONT/src/pages/Sorteo/Sorteo.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import "./Sorteo.scss"
+
+const Sorteo = () => {
+
+  
+  return (
+
+    <>
+    <h1>sorteo</h1>
+
+    <div className='sorteo'></div>
+
+
+
+    </>
+  )
+}
+
+export default Sorteo
