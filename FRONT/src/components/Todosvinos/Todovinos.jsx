34bceb51c420b0dbf5146a1acb5f1c74f4b98985
Wida30 <wida30@yahoo.es>
2022-06-29 20:56:25 +0200

    descripcionback

--- FRONT/src/components/Todosvinos/Todovinos.jsx
+++ FRONT/src/components/Todosvinos/Todovinos.jsx
@@ -1,8 +1,7 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
-
-
+import "./Todovinos.scss";
 
 const Todovinos = () => {
   const [vinos, setVinos] = useState([]);
@@ -17,37 +16,76 @@ const Todovinos = () => {
 
   const [clasificacion, setClasificacion] = useState("");
 
-  const filterVinos = vinos.filter((vino) =>
-    vino.tipo.includes(clasificacion)
-  );
+  const filterVinos = vinos.filter((vino) => vino.tipo.includes(clasificacion));
 
   return (
     <>
       <div className="botones">
-        <button onClick={() => setClasificacion("tinto")}className = "selecionador">tinto</button>
-        <button onClick={() => setClasificacion("blanco")}className = "selecionador">blanco</button>
-        <button onClick={() => setClasificacion("rosado")}className = "selecionador">rosado</button>
-        <button onClick={() => setClasificacion("")} className = "selecionador">Todos</button>
-        <button> <Link to="/vinoform"> inserta vino </Link> </button>
+        <button
+          onClick={() => setClasificacion("tinto")}
+          className="selecionador"
+        >
+          tinto
+        </button>
+        <button
+          onClick={() => setClasificacion("blanco")}
+          className="selecionador"
+        >
+          blanco
+        </button>
+        <button
+          onClick={() => setClasificacion("rosado")}
+          className="selecionador"
+        >
+          rosado
+        </button>
+        <button onClick={() => setClasificacion("")} className="selecionador">
+          Todos
+        </button>
+        <button>
+          {" "}
+          <Link to="/vinoform"> inserta vino </Link>{" "}
+        </button>
       </div>
 
       <div className="mostrando">
+        {filterVinos.length ? (
+          <>
+            {filterVinos.map((vino) => (
+              <Link key={vino._id} to={`${vino._id}`}>
+                <figure className="container">
+                  <h2>{vino.nombre}</h2>
+                  <img src={vino.foto} alt={vino.nombre} className="image" />
 
-      {filterVinos.length ? (
-        <>
-          {filterVinos.map((vino) => (
-
-            <Link key={vino._id} to={`${vino._id}`}>
-            <figure  className="carta">
-              <h2>{vino.nombre}</h2>
-              <img src={vino.foto} alt={vino.nombre} />
-            </figure>
-            </Link>
-          ))}
-        </>
-      ) : (
-        <p>Loadin...</p>
-      )}
+                  <div className="overlay">
+                  {/* <p className="text">
+                     
+                      <strong>tipo:</strong> {vino.tipo}
+                    </p>
+                    <p className="text">
+                  
+                      <strong>uva:</strong> {vino.uva}
+                    </p> */}
+                    <p className="text">
+                    
+                      <strong>descripcion:</strong> {vino.descripcion}
+                    </p>
+                    {/* <p className="text">
+                      
+                      <strong>añada:</strong> {vino.añada}
+                    </p>
+                    <p className="text">
+                    
+                      <strong>precio:</strong> {vino.precio} euros
+                    </p> */}
+                  </div>
+                </figure>
+              </Link>
+            ))}
+          </>
+        ) : (
+          <p>Loadin...</p>
+        )}
       </div>
     </>
   );
